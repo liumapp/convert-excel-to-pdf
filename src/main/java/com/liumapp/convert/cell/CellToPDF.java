2758caaf69ca07ca4e8716c1512a7d30709bd31c
liumapp <liumapp.com@gmail.com>
2018-04-26 17:40:26 +0800

    update cell to pdf

--- src/main/java/com/liumapp/convert/cell/CellToPDF.java
+++ src/main/java/com/liumapp/convert/cell/CellToPDF.java
@@ -1,5 +1,13 @@
 package com.liumapp.convert.cell;
 
+import com.aspose.cells.License;
+import com.aspose.cells.SaveFormat;
+import com.aspose.cells.Workbook;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+
 /**
  * @author liumapp
  * @file CellToPDF.java
@@ -8,4 +16,51 @@ package com.liumapp.convert.cell;
  * @date 4/26/18
  */
 public class CellToPDF {
+
+    private static InputStream license;
+    private static InputStream cells;
+
+    /**
+     * 获取license
+     *
+     * @return
+     */
+    public static boolean getLicense() {
+        boolean result = false;
+        try {
+            license = CellToPDF.class.getClassLoader().getResourceAsStream("license.xml");    // license路径
+//            cells = TestCells.class.getClassLoader().getResourceAsStream("test.xlsx");    // 原始excel路径
+            License aposeLic = new License();
+            aposeLic.setLicense(license);
+            result = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        // 验证License
+        if (!getLicense()) {
+            return;
+        }
+        try {
+            long old = System.currentTimeMillis();
+            Workbook wb = new Workbook(cells);
+            File file = new File("D:\\test.pdf");// 输出路径
+            FileOutputStream fileOS = new FileOutputStream(file);
+
+            wb.save(fileOS, SaveFormat.PDF);
+
+            long now = System.currentTimeMillis();
+            System.out.println("共耗时：" + ((now - old) / 1000.0) + "秒\n\n" + "文件保存在:" + file.getPath());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
